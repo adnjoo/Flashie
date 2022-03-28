d9b7c9aa1c6ce2435a56d776c3a2d916feda30ac
Andrew Njoo <adnjoo@gmail.com>
2022-03-28 16:55:14 -0700

    move folder, add eslint

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const x:Number = 123;
+  return (
+    <div className="App">
+      hi
+      {String(x)}
+    </div>
+  );
+}
+
+export default App;
