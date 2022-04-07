892ff977c5af2bb117dee5525084c2443cba832d
Andrew Njoo <adnjoo@gmail.com>
2022-04-07 07:28:36 -0700

    add files

---
+++ server/controller.js
@@ -0,0 +1,31 @@
+const { Pool } = require("pg");
+const router = require("express").Router();
+require("dotenv").config();
+
+// local use
+const { host, user, database, password, port } = require("./config");
+
+const pool = new Pool({
+  host,
+  user,
+  database,
+  password,
+  port,
+});
+
+// deployment
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
+
+const test = (req, res) => {
+  res.status(200).send("Testing Zen API");
+};
+
+module.exports = {
+  test,
+};
